79894345dc6f7bddf2cd949eb495fb543fa866ef
유종원 <yoojong-won@yujong-won-ui-MacBookPro.local>
2024-06-08 15:52:19 +0900

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["nuxt-lodash"],
+  lodash: {
+    prefix: "_",
+    prefixSkip: ["string"],
+    upperAfterPrefix: false,
+    exclude: ["map"],
+    alias: [
+      ["camelCase", "stringToCamelCase"], // => stringToCamelCase
+      ["kebabCase", "stringToKebab"], // => stringToKebab
+      ["isDate", "isLodashDate"], // => _isLodashDate
+    ],
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import _ from 'lodash'
+const wheelEvent = (event: any) => {
+    console.log('wheel');
+    
+}
+onMounted(() => {
+    console.log('test');
+    document.addEventListener('wheel', _.throttle(wheelEvent, 1000));
+})
+</script>
+<template>
+<div>
+    <div class="container" style="background-color: red;"></div>
+    <div class="container" style="background-color: tomato;"></div>
+    <div class="container" style="background-color: aqua;"></div>
+</div>
+</template>
+<style>
+.container {
+    height: 100vh;
+    width: 100%;
+}
+</style>
